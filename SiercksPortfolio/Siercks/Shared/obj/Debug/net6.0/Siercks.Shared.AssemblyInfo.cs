fdc52c196ea4f956f8885b65be67fbb4bbb82f20
siercks <108698268+siercks@users.noreply.github.com>
2024-03-11 14:15:04 -0500

    new

--- SiercksPortfolio/Siercks/Shared/obj/Debug/net6.0/Siercks.Shared.AssemblyInfo.cs
+++ SiercksPortfolio/Siercks/Shared/obj/Debug/net6.0/Siercks.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Siercks.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14846ad2c5942b7e67dd12ae5ba721c808c0297")]
 [assembly: System.Reflection.AssemblyProductAttribute("Siercks.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Siercks.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
