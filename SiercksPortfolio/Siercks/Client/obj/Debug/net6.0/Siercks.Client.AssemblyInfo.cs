2a1d943b16f6d50dddd18f1ef359edededa02a2c
siercks <108698268+siercks@users.noreply.github.com>
2024-04-08 13:27:44 -0500

    new

--- SiercksPortfolio/Siercks/Client/obj/Debug/net6.0/Siercks.Client.AssemblyInfo.cs
+++ SiercksPortfolio/Siercks/Client/obj/Debug/net6.0/Siercks.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Siercks.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e3ff27bad91451c1b78455597ee270fcd776ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10da691e5a0ba8b120c97ac2511de2c364ef1bb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Siercks.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Siercks.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SiercksPortfolio/Siercks/Shared/obj/Debug/net6.0/Siercks.Shared.AssemblyInfo.cs
+++ SiercksPortfolio/Siercks/Shared/obj/Debug/net6.0/Siercks.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Siercks.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14846ad2c5942b7e67dd12ae5ba721c808c0297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10da691e5a0ba8b120c97ac2511de2c364ef1bb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Siercks.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Siercks.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
