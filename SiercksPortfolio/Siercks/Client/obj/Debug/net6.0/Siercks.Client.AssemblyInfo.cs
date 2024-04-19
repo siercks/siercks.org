e404ca7eff88d966c6a2796de84357c6ed81f81f
siercks <108698268+siercks@users.noreply.github.com>
2024-04-19 06:47:30 -0500

    April 2024

--- SiercksPortfolio/Siercks/Client/obj/Debug/net6.0/Siercks.Client.AssemblyInfo.cs
+++ SiercksPortfolio/Siercks/Client/obj/Debug/net6.0/Siercks.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Siercks.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10da691e5a0ba8b120c97ac2511de2c364ef1bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a1d943b16f6d50dddd18f1ef359edededa02a2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Siercks.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Siercks.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
